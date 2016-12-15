533880c645f6f099cd10f83b42d9fedead3debb3
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-16 00:44:07 +0100

    Refactor function.

--- src/client/boundary/render-dom.js
+++ src/client/boundary/render-dom.js
@@ -10,19 +10,19 @@ const domTarget = document.getElementById('root')
 let rehydrate = true
 
 function clearStaticRender () {
-
-}
-
-function onClientStateRepresentation (input) {
-  const view = stateRepresentation({model: input.model})
   if (rehydrate) {
     const root = document.querySelector('#root')
     while (root.firstChild) {
       root.removeChild(root.firstChild)
     }
   }
-  morphdom(domTarget, h('div#root', view))
   rehydrate = false
+}
+
+function onClientStateRepresentation (input) {
+  const view = stateRepresentation({model: input.model})
+  clearStaticRender()
+  morphdom(domTarget, h('div#root', view))
   log('rendered')
 }
 
