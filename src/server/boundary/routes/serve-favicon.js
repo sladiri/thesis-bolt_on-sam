f77fd0223821aea6de0b3425b40304cf798336ea
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-18 13:10:23 +0100

    Remove unused favicon route.

--- src/server/boundary/routes/serve-favicon.js
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-import favicon from 'koa-favicon'
-
-const iconPath = '../../public/favicon/foot/favicon.ico'
-const options = {maxAge: 1000 * 60 * 10}
-
-export default favicon(path.join(__dirname, iconPath), options)
