dd250780fdb48b3e939189bd9d198a5334d88402
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-28 12:30:21 +0100

    Use shared to-bus-adapter.

--- src/client/boundary/bus-adapters/sse-to-bus-adapter.js
+++ src/client/boundary/bus-adapters/sse-to-bus-adapter.js
@@ -1,5 +1,6 @@
 import {logConsole} from '../../../shared/boundary/logger'
-import {getSink} from '../../../shared/boundary/connect-postal'
+import {toBusAdapter} from '../../../shared/boundary/connect-postal'
+import {pipe, prop} from 'ramda'
 
 const logName = 'sse-to-bus-adapter'
 const log = logConsole(logName)
@@ -9,21 +10,13 @@ const logMessage = event =>
 
 export default function sseToBusAdapter (url) {
   let source = new EventSource(url)
-  let sinks = {}
-
-  function messageHandler (event) {
-    log('got message', JSON.stringify(event))
-    const payload = JSON.parse(event.data)
-    const target = payload.envelope.topic
-    sinks = sinks || {}
-    const sink = sinks[target] = sinks[target] || getSink({targets: [target]})
-    sink(payload.data)
-  }
+  const sinks = {}
+  const toBus = pipe(prop('data'), toBusAdapter({sinks, logTag: logName}))
 
   function openHandler (event) {
     log('open', ...logMessage(event))
     source.removeEventListener('open', openHandler)
-    source.addEventListener('message', messageHandler)
+    source.addEventListener('message', toBus)
   }
 
   function errorHandler (event) {
@@ -33,7 +26,9 @@ export default function sseToBusAdapter (url) {
       log('error', ...logMessage(event))
     }
     source = undefined
-    sinks = undefined
+    Object.keys(sinks).forEach(sink => {
+      delete sinks[sink]
+    })
   }
 
   source.addEventListener('open', openHandler)
