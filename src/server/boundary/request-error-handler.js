3cd3b77b2bdff20f8663886325ecc640678f92eb
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-28 15:24:52 +0100

    Fix request error handler.

--- src/server/boundary/request-error-handler.js
+++ src/server/boundary/request-error-handler.js
@@ -1,8 +1,17 @@
-export default async function errorHandler ({body, status}, next) {
+import {logConsole} from '../../shared/boundary/logger'
+
+const logName = 'repsonse-logger'
+const log = logConsole(logName)
+
+export default async function errorHandler (ctx, next) {
   try {
     await next()
   } catch (err) {
-    body = { message: err.message }
-    status = err.status || 500
+    const status = err.status || 500
+
+    log('error', status, err.message)
+
+    ctx.status = status
+    ctx.body = { message: err.message }
   }
 }
