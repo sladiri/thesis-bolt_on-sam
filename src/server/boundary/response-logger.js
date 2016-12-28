0b5b146d8b446769ecf057310f1760665c80f102
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-28 15:36:59 +0100

    Do not log Systemjs requests.

--- src/server/boundary/response-logger.js
+++ src/server/boundary/response-logger.js
@@ -2,19 +2,24 @@ import {logConsole} from '../../shared/boundary/logger'
 
 const logName = 'response-logger'
 
+const isJspmRequest = url =>
+  url.indexOf('public/jspm_packages') !== -1 || url.indexOf('public/src')
+
 export default async function responseLogger (ctx, next) {
   const {method, url} = ctx
 
   const start = new Date()
   const log = logConsole(logName, `(${method} ${url})`)
-  log('start')
+  if (!isJspmRequest(url)) {
+    log('start')
+  }
 
   await next()
 
   const elapsed = new Date() - start
   ctx.set('X-Response-Time', `${elapsed}ms`)
 
-  if (!url.startsWith('/public/jspm_packages')) {
+  if (!isJspmRequest(url)) {
     log(`end, ${elapsed}ms elapsed`)
   }
 }
