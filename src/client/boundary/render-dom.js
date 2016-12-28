15ba4456ddec9af5fd438470389be585e8e40ada
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-28 15:25:19 +0100

    Refactor.

--- src/client/boundary/render-dom.js
+++ src/client/boundary/render-dom.js
@@ -1,18 +1,17 @@
 import {logConsole} from '../../shared/boundary/logger'
-import {render as _render} from 'inferno-dom'
+import {render} from 'inferno-dom'
 import {__, curry, pipe} from 'ramda'
 import stateRepresentation, {validate} from '../../shared/boundary/state-representation'
 
-const logName = 'render_dom'
+const logName = 'render-dom'
 const log = logConsole(logName)
 
-const render = curry(_render)
 const domTarget = document.getElementById('root')
 
 function onClientStateRepresentation (input) {
   pipe(
     stateRepresentation,
-    render(__, domTarget)
+    curry(render)(__, domTarget)
   )(input)
 
   log('rendered')
