53790a20985819eb14759971c74658a38c03307c
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-29 14:20:07 +0100

    Formatting.

--- src/server/boundary/wire.js
+++ src/server/boundary/wire.js
@@ -1,10 +1,13 @@
 import Koa from 'koa'
+
+import prettyJSON from 'koa-json'
 import errorHandler from './request-error-handler'
 import responseLogger from './response-logger'
-import serveFiles from './routes/serve-files'
-import renderStringOptions, {renderString} from './routes/render-string'
-import prettyJSON from 'koa-json'
+
 import mount from 'koa-mount'
+
+import serveFiles from './routes/serve-files'
+
 import * as test from './routes/test'
 
 import busToSse from './bus-adapters/bus-to-sse-adapter'
@@ -14,19 +17,23 @@ import {connect} from '../../shared/boundary/connect-postal'
 import createServer from './server'
 import modelOptions from '../../shared/control/model'
 import actionOptions from '../../shared/control/actions'
+import renderStringOptions, {renderString} from './routes/render-string'
 
 function createApp () {
   const app = new Koa()
 
+  app.use(prettyJSON())
   app.use(errorHandler)
   app.use(responseLogger)
+
   app.use(mount('/public', serveFiles('/public')))
-  app.use(prettyJSON())
+
   Object.keys(test).forEach(key => {
     app.use(mount(`/test/${key}`, test[key]))
   })
-  app.use(mount('/actions', httpToBus))
+
   app.use(mount('/sse', busToSse(['stateRepresentation'])))
+  app.use(mount('/actions', httpToBus))
   app.use(mount(renderString))
 
   return app
