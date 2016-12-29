e019c154dda3fd00b61b1b6f76965aacf20a9cab
Slađan Ristić <sladiri@users.noreply.github.com>
2016-12-29 03:00:16 +0100

    Remove action interval.

--- src/client/boundary/wire.js
+++ src/client/boundary/wire.js
@@ -8,8 +8,8 @@ connect(renderOptions)
 sseToBus('/sse')
 busToHttp({url: '/actions', targets: ['actions']})
 
-const actionSink = getSink({targets: ['actions'], logTag: 'client-wire'})
-actionSink(null)
-setInterval(() => {
-  actionSink(null)
-}, 100)
+getSink({targets: ['actions'], logTag: 'client-wire'})(null)
+// const actionSink = getSink({targets: ['actions'], logTag: 'client-wire'})
+// setInterval(() => {
+//   actionSink(null)
+// }, 100)
